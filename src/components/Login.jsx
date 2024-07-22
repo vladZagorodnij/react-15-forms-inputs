90e7821d3107c0df2393870cc009bced297a9530
Vladyslav Zahorodnii <vladyslav.zahorodnii@valtech.com>
2024-07-22 17:01:34 +0300

    Store input values in different state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,53 @@
+import {useState} from "react";
+
 export default function Login() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('Submitted');
+    }
+
+    function handleEmailChange(event) {
+        setEnteredEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setEnteredPassword(event.target.value);
+    }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+              id="email"
+              type="email"
+              name="email"
+              onChange={handleEmailChange}
+              value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={handlePasswordChange}
+              value={enteredPassword}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button onClick={handleSubmit} className="button">Login</button>
       </p>
     </form>
   );
