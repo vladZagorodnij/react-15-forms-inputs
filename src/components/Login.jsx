d7aa34eb91659b6d6d09e57d021d2484dcdff2ce
Vladyslav Zahorodnii <vladyslav.zahorodnii@valtech.com>
2024-07-22 20:14:35 +0300

    single input validation. if symbol exists

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,8 @@ export default function Login() {
         password: '',
     });
 
+    const emailIsValid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+
     function handleSubmit(event) {
         event.preventDefault();
         console.log('Submitted');
@@ -18,42 +20,45 @@ export default function Login() {
         }))
     }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-              id="email"
-              type="email"
-              name="email"
-              onChange={(event) => {
-                  handleInputChange('email', event.target.value)
-              }}
-              value={enteredValues.email}
-          />
-        </div>
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        onChange={(event) => {
+                            handleInputChange('email', event.target.value)
+                        }}
+                        value={enteredValues.email}
+                    />
+                    <div className="control-error">
+                        {emailIsValid && <p>Please enter a valid Email address.</p>}
+                    </div>
+                </div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-              id="password"
-              type="password"
-              name="password"
-              onChange={(event) => {
-                  handleInputChange('password', event.target.value)
-              }}
-              value={enteredValues.password}
-          />
-        </div>
-      </div>
+                <div className="control no-margin">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        onChange={(event) => {
+                            handleInputChange('password', event.target.value)
+                        }}
+                        value={enteredValues.password}
+                    />
+                </div>
+            </div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button onClick={handleSubmit} className="button">Login</button>
-      </p>
-    </form>
-  );
+            <p className="form-actions">
+                <button className="button button-flat">Reset</button>
+                <button onClick={handleSubmit} className="button">Login</button>
+            </p>
+        </form>
+    );
 }
