d5819ccc5fb0a5349772f2da91dd6ebaf50e34ea
Vladyslav Zahorodnii <vladyslav.zahorodnii@valtech.com>
2024-07-22 17:09:27 +0300

    Store input values in one state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,21 @@
 import {useState} from "react";
 
 export default function Login() {
-    const [enteredEmail, setEnteredEmail] = useState('');
-    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredValues, setEnteredValues] = useState({
+        email: '',
+        password: '',
+    });
 
     function handleSubmit(event) {
         event.preventDefault();
         console.log('Submitted');
     }
 
-    function handleEmailChange(event) {
-        setEnteredEmail(event.target.value);
-    }
-
-    function handlePasswordChange(event) {
-        setEnteredPassword(event.target.value);
+    function handleInputChange(identifier, value) {
+        setEnteredValues((prevValues) => ({
+            ...prevValues,
+            [identifier]: value,
+        }))
     }
 
   return (
@@ -28,8 +29,10 @@ export default function Login() {
               id="email"
               type="email"
               name="email"
-              onChange={handleEmailChange}
-              value={enteredEmail}
+              onChange={(event) => {
+                  handleInputChange('email', event.target.value)
+              }}
+              value={enteredValues.email}
           />
         </div>
 
@@ -39,8 +42,10 @@ export default function Login() {
               id="password"
               type="password"
               name="password"
-              onChange={handlePasswordChange}
-              value={enteredPassword}
+              onChange={(event) => {
+                  handleInputChange('password', event.target.value)
+              }}
+              value={enteredValues.password}
           />
         </div>
       </div>
